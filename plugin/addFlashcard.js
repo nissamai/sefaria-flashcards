37c6b20f1f74350242550658ebdff60da7fde754
nissamai <nmai@mathematica-mpr.com>
2021-02-14 11:31:44 -0500

    attempt to make context menu persistent between chrome sessions

--- plugin/addFlashcard.js
+++ plugin/addFlashcard.js
@@ -10,31 +10,14 @@ function createFlashcard(info) {
   chrome.tabs.create({
     url: chrome.extension.getURL(`flashcards-manager/index.html#/add-card/${headword}/${lookupRef}`)
   });
-//   Promise.all([getDefinition(wordInfo), getContext(wordInfo)])
-//     .then(responses =>
-//       Promise.all(responses.map(res => res.json()))
-//     )
-//     .then(res => {
-//       console.log(res[0]);
-//       console.log(res[1]);
-//       // open new tab
-//       // send it data
-//     })
-//     .catch(err => {
-//       console.log(err); // TODO: handle failed to get flashcard 
-//     })
-// }
-
 }
 
 var supportedPages = ["https://*.sefaria.org/*", "https://mg.alhatorah.org/*"];
 
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.contextMenus.create({
+chrome.contextMenus.create({
     "id": "sampleContextMenu",
     "title": "Create Flashcard",
     "contexts": ["selection"],
     "onclick": createFlashcard,
     "documentUrlPatterns": supportedPages
   });
-});
